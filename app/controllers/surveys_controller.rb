7146d65464e02d3c9a106e13f0e6e59f4f78e231
Goncalo Mendes <goncalocmendes04@gmail.com>
2024-11-27 20:30:24 +0000

    Add styling to surveys, Add generic step

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,35 +1,42 @@
-
 class SurveysController < ApplicationController
-  def step_one
-    @form_data = session[:form_data] || {}
-    @step = 0
-    @total_steps = 0
+  before_action :set_form_data, only: %i[step step_submit]
+
+  def step
+    @step = params[:step].to_i
+    @total_steps = 4
   end
 
-  def step_two
-    @form_data = session[:form_data] || {}
+  def step_submit
+    @step = params[:step].to_i
     @form_data.merge!(survey_params) if params[:survey]
     session[:form_data] = @form_data
-  end
 
-  def step_three
-    @form_data = session[:form_data] || {}
-    @form_data.merge!(survey_params) if params[:survey]
-    session[:form_data] = @form_data
-  end
-
-  def step_four
-    @form_data = session[:form_data] || {}
-    @form_data.merge!(survey_params) if params[:survey]
-    session[:form_data] = @form_data
+    if params[:direction] == "previous"
+      redirect_to survey_step_path(step: @step - 1)
+    elsif @step < 3
+      redirect_to survey_step_path(step: @step + 1)
+    else
+      redirect_to submit_survey_path
+    end
   end
 
   def submit_survey
     @form_data = session[:form_data] || {}
     @form_data.merge!(survey_params) if params[:survey]
+
+    if Survey.create(@form_data)
+      session.delete(:form_data)
+      redirect_to thank_you_path, notice: "Survey submitted successfully!"
+    else
+      redirect_to step_submit_path(step: 4), alert: "There was an error submitting your survey."
+    end
   end
 
   private
+
+  def set_form_data
+    @form_data = session[:form_data] || {}
+  end
 
   def survey_params
     params.require(:survey).permit(:goals, :preferences, :restrictions, :servings)
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,20 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
+  # get "surveys", to: "surveys#new"
+  # get "surveys", to: "surveys#create"
   # Defines the root path route ("/")
   # root "posts#index"
+
+  resources :surveys, only: [] do
+    collection do
+      match 'step/:step',
+        to: 'surveys#step_submit',
+        via: [:post],
+        as: :step_submit
+
+      get 'step/:step', to: 'surveys#step', as: :survey_step
+      post 'submit', to: 'surveys#submit_survey', as: :submit_survey
+    end
+  end
 end
