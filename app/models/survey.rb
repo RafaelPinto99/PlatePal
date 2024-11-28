2e74d013be5f546c3e8388170a5d93260a1e2f78
Goncalo Mendes <goncalocmendes04@gmail.com>
2024-11-28 15:27:16 +0000

    add enums to survey model

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,3 +1,9 @@
 class Survey < ApplicationRecord
   belongs_to :user
+
+  validates :goals, :preferences, :restrictions, :servings, presence: true
+
+  enum goal: { save_time: 0, healthy: 1, diet: 2 }
+  enum availability: { limited: 0, open: 1 }
+  enum diet: { no_restriction: 0, vegetarian: 1, vegan: 2, gluten_free: 3, dairy_free: 4, low_carb: 5 }
 end
