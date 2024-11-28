ce5f6c2de7d0a15d52c4a6f79e6fdfa100a1654c
Goncalo Mendes <goncalocmendes04@gmail.com>
2024-11-28 17:40:24 +0000

    add labels to survey options

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -8,23 +8,33 @@ class Survey < ApplicationRecord
   enum diet: { no_restriction: 0, vegetarian: 1, vegan: 2, gluten_free: 3, dairy_free: 4, low_carb: 5 }
 
   def self.goal_options
-    # This method returns an array of arrays, where each sub-array contains a symbol and a humanized string.
-    # The symbol is the key of the enum, and the humanized string is used as the label in the survey form.
-    Survey.goals.keys.map { |goal| [goal.to_sym, goal.humanize] }
+      # This method returns an array of arrays, where each sub-array contains a symbol and a humanized string.
+      # The symbol is the key of the enum, and the humanized string is used as the label in the survey form.
+      # Survey.goals.keys.map { |goal| [goal.to_sym, goal.humanize] }
 
-    # To customize the labels, you can use the following syntax:
-    # [
-    #   [:save_time, "Save time during the week"],
-    #   [:healthy, "Create healthy, balanced meals"],
-    #   [:diet, "Stick to a specific diet"]
-    # ]
+      # To customize the labels, you can use the following syntax:
+     [
+       [:save_time, "Save time during the week"],
+       [:healthy, "Create healthy, balanced meals"],
+       [:diet, "Stick to a specific diet"]
+     ]
   end
 
   def self.availability_options
-    Survey.availabilities.keys.map { |availability| [availability.to_sym, availability.humanize] }
+    [
+      [:limited, "I need quick, under 30-minute recipes"],
+      [:open, "I can dedicate more time to cooking"]
+    ]
   end
 
   def self.diet_options
-    Survey.diets.keys.map { |diet| [diet.to_sym, diet.humanize] }
+    [
+      [:no_restriction, "We eat everything"],
+      [:vegetarian, "Vegetarian"],
+      [:vegan, "Vegan"],
+      [:gluten_free, "Gluten-free"],
+      [:dairy_free, "Dairy-free"],
+      [:low_carb, "Low-carb"]
+    ]
   end
 end
