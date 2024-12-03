f1906bf1c0843a99ec8e7724ea7285dc2b82d96b
Rafael Pinto <pintorafael1999@gmail.com>
2024-12-03 09:15:49 +0000

    Last page coiso

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,5 +1,4 @@
 class PlansController < ApplicationController
-
   def index
     @plans = Plan.where(user_id: current_user.id)
   end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,5 @@
 class Recipe < ApplicationRecord
   has_one_attached :photo
+  # has_many :plan_recipes
   has_many :plans, through: :plan_recipes
 end
