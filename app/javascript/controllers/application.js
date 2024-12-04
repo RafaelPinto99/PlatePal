bc83983e3dfb66f339334d8219d026b87f5befd5
Carlota Salema <carlota.salema.98@gmail.com>
2024-12-04 17:27:59 +0000

    delete

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,11 +1,8 @@
 import { Application } from "@hotwired/stimulus";
-import RecipeList from "./recipe_list_controller";
 
 const application = Application.start()
 
 application.debug = false
 window.Stimulus = application
-
-Stimulus.register("recipes-list", RecipeList);
 
 export { application}
