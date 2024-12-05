41fd01136515cf3a86271a41bb35c9451955ef8d
Rafael Pinto <pintorafael1999@gmail.com>
2024-12-05 10:08:58 +0000

    Bue de cenas

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    @plan = Plan.first # Fetch the first Plan or choose a specific one
+    @plan_recipes = @plan.plan_recipes.limit(2) # Fetch only two recipes for this plan
   end
 end
