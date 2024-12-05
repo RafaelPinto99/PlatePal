f73836fd3e1d407bc98342671d8c19779d7cfa31
Rita Rodrigues <rita.gpr@gmail.com>
2024-12-05 10:48:56 +0000

    fixes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    @recipes = Recipe.all # Fetch the first Plan or choose a specific one
+    @plan_recipes = @recipes.limit(2)
   end
 end
